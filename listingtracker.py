ad21718f61278b32b8956f178dc0c4fd1c33b516
iragamagoori <iragamagoori1@gmail.com>
2018-04-03 21:46:54 -0400

    more work on ListingTracker

--- listingtracker.py
+++ listingtracker.py
@@ -1,14 +1,49 @@
+import datetime
+
+class ListingEvent(object):
+    _epoch = datetime.datetime.utcfromtimestamp(0)
+
+    def __init__(self, submission, new_position, prev_position, t=None):
+        self.submission = submission
+        self.new_position = new_position
+        self.prev_position = prev_position
+        if t is None:
+            t = datetime.datetime.now()
+        self.time = t
+
+    def json_obj(self):
+        return {
+            'type': 'listing',
+            'timestamp': (self.time - self._epoch).total_seconds(),
+            'id': self.submission.fullname,
+            'new_position': self.new_position,
+            'prev_position': self.prev_position,
+        }
+
 class ListingTracker(object):
-    def __init__(self, list_fn):
+    def __init__(self, list_fn, n=25):
         self.list_fn = list_fn
+        self.n = n
+        self.prev_positions = {}
         self.seen = set()
 
     def check(self):
-        new_submissions = []
+        events = []
+        next_positions = {}
 
-        for i,submission in enumerate(self.list_fn(limit=25)):
-            if submission.fullname not in self.seen:
-                new_submissions.append((i, submission))
-                self.seen.add(submission.fullname)
+        seen = set()
+        for i,submission in enumerate(self.list_fn(limit=self.n)):
+            seen.add(submission.fullname)
 
-        return new_submissions
+            prev_position = self.prev_positions.get(submission.fullname, -1)
+            if i != prev_position:
+                events.append(ListingEvent(submission, i+1, prev_position+1))
+
+            next_positions[submission.fullname] = i
+
+        for fullname in self.prev_positions:
+            if fullname not in seen:
+                events.append(ListingEvent(submission, 0, prev_position+1))
+
+        self.prev_positions = next_positions
+        return events
