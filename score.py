725ded8c49a65c25ef5c5920ddb4cf1b3f135e5f
iragamagoori <iragamagoori1@gmail.com>
2018-04-04 23:06:34 -0400

    fix handling of empty list

--- score.py
+++ score.py
@@ -1,9 +1,10 @@
 def score(r, fullnames, smooth_count = 5):
     scores = dict([(fullname, 0) for fullname in fullnames])
 
-    for i in range(smooth_count):
-        for info in r.info(fullnames=fullnames):
-            scores[info.fullname] += info.score
+    if fullnames:
+        for i in range(smooth_count):
+            for info in r.info(fullnames=fullnames):
+                scores[info.fullname] += info.score
 
     for id in scores.keys():
         scores[id] /= float(smooth_count)
